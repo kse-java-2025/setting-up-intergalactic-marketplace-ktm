c4728d7b159dc16ba4b9a9abfbbf7e52d5d07002
koalla05 <kov.koalla@gmail.com>
2025-11-09 09:08:04 +0200

    Fix: remove extra line

--- spacecatsmarket/src/main/java/com/example/spacecatsmarket/service/mapper/CartMapper.java
+++ spacecatsmarket/src/main/java/com/example/spacecatsmarket/service/mapper/CartMapper.java
@@ -3,12 +3,9 @@ package com.example.spacecatsmarket.service.mapper;
 import com.example.spacecatsmarket.domain.Cart;
 import com.example.spacecatsmarket.dto.cart.CartDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring", uses = { ProductMapper.class })
 public interface CartMapper {
-    CartMapper INSTANCE = Mappers.getMapper(CartMapper.class);
-
     CartDto toDto(Cart cart);
     Cart toEntity(CartDto cartDto);
 }
--- spacecatsmarket/src/main/java/com/example/spacecatsmarket/service/mapper/ProductMapper.java
+++ spacecatsmarket/src/main/java/com/example/spacecatsmarket/service/mapper/ProductMapper.java
@@ -4,14 +4,11 @@ import com.example.spacecatsmarket.domain.Product;
 import com.example.spacecatsmarket.dto.product.ProductDto;
 import com.example.spacecatsmarket.dto.product.ProductEntry;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
-    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
-
     ProductDto toDto(Product product);
     Product toEntity(ProductDto productDto);
     ProductEntry toEntry(Product product);
